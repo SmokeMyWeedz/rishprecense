9a434f66d99d75b17f06c94c8b9c13f7ed977cfd
SmokeMyWeedz <54533169+SmokeMyWeedz@users.noreply.github.com>
2020-03-23 05:10:37 +0000

    Add files via upload

---
+++ Installe_all.bat
@@ -0,0 +1,33 @@
+@shift /0
+@ECHO OFF
+title Custom Precense
+color a
+echo ........./\_\
+echo ......../././_
+echo ......././_/\.\
+echo ......_\.\/..\.\
+echo ...../\.\../\.\_\
+echo .....\.\/..\.\/_/
+echo ......\../\.\_\
+echo .......\/_/././
+echo ........./././
+echo .........\/_/
+
+
+pause
+color 2 
+cls
+echo installation du rishpresence cuestom 
+echo ┏┓︱︱ ︱︱︱︱ ︱︱┏━━━┓  ┏┓︱ ︱︱︱︱
+echo ┃┃︱︱ ︱︱︱︱ ︱︱┃┏━┓┃ ┏┛┗┓ ︱︱︱︱
+echo ┃┗━┓ ┏━━┓ ┏━┓ ┏━┓ ┗┛┏┛┃ ┗┓┏┛ ┏━━┓
+echo ┃┏┓┃ ┃┃━┫ ┃┏┛ ┃┏┛ ┏┓┗┓┃  ┃┃  ┃┏┓┃
+echo ┃┗┛┃ ┃┃━┫ ┃┃  ┃┃  ┃┗━┛┃  ┃┗┓ ┃┏┓┃
+echo ┗━━┛ ┗━━┛ ┗┛  ┗┛  ┗━━━┛  ┗━┛ ┗┛┗┛
+
+pause
+cls
+
+@echo off
+color b
+npm install
